341acb860007cd2a008b66a7f4bccb1d8b1a3d1c
Denis Buzmakov <denis@bpdigital.ru>
2017-04-09 19:23:16 +0500

    добавлен Crashlytics

--- app/src/main/java/com/bacecek/translate/App.java
+++ app/src/main/java/com/bacecek/translate/App.java
@@ -2,6 +2,8 @@ package com.bacecek.translate;
 
 import android.app.Application;
 import com.bacecek.translate.data.db.PrefsManager;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import timber.log.Timber;
 import timber.log.Timber.DebugTree;
@@ -16,6 +18,7 @@ public class App extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		Fabric.with(this, new Crashlytics());
 
 		PrefsManager.init(this);
 		Realm.init(this);
